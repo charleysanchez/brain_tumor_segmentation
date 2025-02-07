05fe260a06ba5dec47a6cc95d7008531fa2dde7f
Charley Sanchez <charleysanchez7@gmail.com>
2025-02-07 03:09:42 -0800

    begin to preprocess into data_load.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,22 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 
-images = []
-masks = []
-for i in range(155):
-    file_path = f'../data/raw/Brats2020/BraTS2020_training_data/content/data/volume_1_slice_{i}.h5'
-    with h5py.File(file_path, 'r') as f:
-        image_data = f["image"][:]
-        mask_data = f["mask"][:]
-        
-
-        images.append(image_data)
-        masks.append(mask_data)
 
 
-images = np.array(images)
-masks = np.array(masks)
-print("shape of image:", images.shape)
-print("shape of mask:", masks.shape)
+def load_data():
+    images = []
+    masks = []
+    for i in range(155):
+        file_path = f'../data/raw/Brats2020/BraTS2020_training_data/content/data/volume_1_slice_{i}.h5'
+        with h5py.File(file_path, 'r') as f:
+            image_data = f["image"][:]
+            mask_data = f["mask"][:]
+            
 
-images = images.astype(np.float32)
+            images.append(image_data)
+            masks.append(mask_data)
+
+
+    images = np.array(images)
+    masks = np.array(masks)
+    return images, masks
+
+
+def preprocess_image_masks(images, masks):
+    # make sure all image values are represented by float32 for numerical stability
+    images = images.astype(np.float32)
+
+    # establish the percentiles per channel (channel is axis 3)
+    p_low = np.percentile(images, 1, axis=(0, 1, 2), keepdims=True)
+    p_high = np.percentile(images, 99, axis=(0, 1, 2), keepdims=True)
+
+    # truncate all values of images to the low and high percentiles to get rid of outliers
+    images = np.clip(images, p_low, p_high)
+    images = (images - p_low) / (p_high - p_low)
+
+    # the masks should simply be categorical
+    masks = masks.astype(np.uint8)
+
+    return images, masks
+
+images, masks = load_data()
+images, masks = preprocess_image_masks(images, masks)
