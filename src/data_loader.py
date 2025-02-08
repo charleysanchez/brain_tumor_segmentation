a6aad1418c8f95989f79c21a56274e73566ce823
Charley Sanchez <charleysanchez7@gmail.com>
2025-02-08 01:42:42 -0800

    deleted redundant normalization function

--- src/data_loader.py
+++ src/data_loader.py
@@ -29,25 +29,6 @@ def load_data(vol_range):
     return images, masks
 
 
-def normalize_mri(images: np.ndarray, masks: np.ndarray, percentage_low=1, percentage_high=99) -> np.ndarray:
-    """Normalize multi-modal MRI volumes"""
-    # make sure all image values are represented by float32 for numerical stability
-    images = images.astype(np.float32)
-
-    # establish the percentiles per channel (channel is axis 3)
-    p_low = np.percentile(images, percentage_low, axis=(0, 1, 2), keepdims=True)
-    p_high = np.percentile(images, percentage_high, axis=(0, 1, 2), keepdims=True)
-
-    # truncate all values of images to the low and high percentiles to get rid of outliers
-    images = np.clip(images, p_low, p_high)
-    images = (images - p_low) / (p_high - p_low)
-
-    # the masks should simply be categorical
-    masks = masks.astype(np.uint8)
-
-    return images, masks
-
-
 def convert_to_pytorch_tensors(images, masks):
     # Permute the array to (N, C, H, W) format where N=155, C=4 (modalities), H=240, W=240
     tensor_images = torch.from_numpy(images).permute(0, 3, 1, 2)
@@ -59,15 +40,23 @@ def convert_to_pytorch_tensors(images, masks):
 
 
 def transform(images, masks):
-    tranforms = A.Compose([
-        A.HorizontalFlip(p=0.5)
+    transforms = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.RandomRotate90(p=0.5),
+        A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.2),
+        A.GaussNoise(var_limit=(0.01, 0.05), p=0.2),
+        A.Normalize(mean=[0.5], std=[0.5]),
+        ToTensorV2()
     ])
+
+    transformed = transforms(image=images, mask=masks)
+    return transformed
 
 
 def preprocess_data(vol_range):
     images, masks = load_data(images, masks, vol_range)
-    images, masks = normalize_mri(images, masks)
     tensor_images, tensor_masks = convert_to_pytorch_tensors(images, masks)
+    image_transformed, mask_transformed = 
     return tensor_images, tensor_masks
 
 
